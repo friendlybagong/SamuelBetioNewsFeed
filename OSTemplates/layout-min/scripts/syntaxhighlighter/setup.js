3f845fdd9437ca64d0ba8a665e33f9aa40314bdd
Samuel Rapana Betio <samuelbetio@users.noreply.github.com>
2017-09-21 13:14:31 +0800

    Add files via upload

---
+++ OSTemplates/layout-min/scripts/syntaxhighlighter/setup.js
@@ -0,0 +1,6 @@
+SyntaxHighlighter.defaults['gutter'] = false;
+SyntaxHighlighter.defaults['stripBrs'] = true;
+SyntaxHighlighter.defaults['toolbar'] = false;
+SyntaxHighlighter.defaults['auto-links'] = false;
+SyntaxHighlighter.config.tagName = "code";
+SyntaxHighlighter.all();
