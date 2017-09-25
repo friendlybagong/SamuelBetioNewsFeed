dbf925bc585908638f832a86043380b7a8e5d73c
Samuel Rapana Betio <samuelbetio@users.noreply.github.com>
2017-09-25 13:56:26 +0800

    Create columns.php

---
+++ assets/files/free-css-templates/preview/page218/lumino/pages/columns.php
@@ -0,0 +1,211 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Free CSS | Free CSS Templates | Demo of the HTML CSS Template Lumino | Pages | Columns</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/plugins.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+<!--[if lt IE 9]>
+<script src="../assets/js/html5shiv.min.js"></script>
+<script src="../assets/js/respond.min.js"></script>
+<![endif]-->
+<style type="text/css">
+#freecssfooter{display:block;width:100%;padding:120px 0 20px;overflow:hidden;background-color:transparent;z-index:5000;text-align:center;}
+#freecssfooter div#fcssholder div{display:none;}
+#freecssfooter div#fcssholder div:first-child{display:block;}
+#freecssfooter div#fcssholder div:first-child a{float:none;margin:0 auto;}
+</style></head>
+<body id="top" class="page-2">
+<script type="text/javascript">
+(function(){
+  var bsa = document.createElement('script');
+     bsa.type = 'text/javascript';
+     bsa.async = true;
+     bsa.src = '//s3.buysellads.com/ac/bsa.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+})();
+</script>
+<div class="tm-navbar-container tm-navbar-container-dark">
+  <nav class="navbar navbar-full navbar-fixed-top bg-inverse">
+    <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#tmNavbar">&#9776;</button>
+    <div class="collapse navbar-toggleable-sm" id="tmNavbar">
+      <ul class="nav navbar-nav">
+        <li class="nav-item"><a href="https://samuelbetio.github.io/SamuelBetioNewsFeed/free-css-templates" class="nav-link external">Home</a></li>
+        <li class="nav-item"><a href="#top" class="nav-link">One Column</a></li>
+        <li class="nav-item"><a href="#tm-section-3" class="nav-link">Two Column</a></li>
+        <li class="nav-item"><a href="#tm-section-4" class="nav-link">Three Column</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
+<div class="container-fluid">
+  <div class="row">
+    <div id="tm-section-1" class="tm-section">
+      <div class="col-md-12">
+        <h1 class="text-xs-center blue-text tm-page-2-title">Lumino Bootstrap 4.0 Columns</h1>
+        <p class="text-xs-center tm-page-2-p tm-page-2-subtitle"> Etiam at rhoncus nisl. Nunc rutrum ac ante euismod cursus. <br>
+          Suspendisse imperdiet feugiat massa nec iaculis.</p>
+        <img src="../assets/img/tm-1800x600-01.jpg" class="img-fluid tm-banner-img" alt="website template image"></div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="tm-section" id="tm-section-2">
+      <div class="col-md-12">
+        <p class="text-left tm-description"> Lumino theme is a Bootstrap 4.0 mobile compatible layout for your website. Cras dolor neque, mollis et tortor eget, ornare hendrerit lectus. Donec condimentum leo ut elementum consequat. Sed condimentum sagittis neque in iaculis. Duis quis libero nec mauris porta luctus et sit amet turpis. Proin auctor tortor quis ipsum dignissim, quis congue tortor.</p>
+        <p class="text-left tm-description"> Aliquam non vestibulum mi, sed volutpat ipsum. Nunc ultricies quam id mi semper, vitae mattis mi iaculis. Nullam tincidunt vehicula turpis at porttitor. Sed bibendum odio non maximus suscipit. Pellentesque consectetur orci id rutrum lacinia.</p>
+        <div class="tm-flex-center"><a href="https://samuelbetio.github.io/SamuelBetioNewsFeed/free-css-templates" class="btn btn-default btn-lg tm-gray-btn">Large</a> <a href="https://samuelbetio.github.io/SamuelBetioNewsFeed/free-css-templates" class="btn btn-default btn-lg tm-gray-bordered-btn">Large</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="tm-section" id="tm-section-3">
+      <div class="col-md-6 margin-bottom-sm-8">
+        <div class="tm-2-col-box-left"><img src="../assets/img/tm-700x350-01.jpg" alt="website template image" class="img-fluid tm-img-2-col">
+          <p class="text-left tm-description"> Proin auctor tortor quis ipsum dignissim, quis congue tortor scelerisque. Etiam malesuada efficitur elit, ut pellentesque mi pellentesque ut.</p>
+          <p class="text-left tm-description"> Nullam tincidunt vehicula turpis at porttitor. Sed bibendum odio non maximus suscipit. Pellentesque consectetur orci id rutrum lacinia.</p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div><img src="../assets/img/tm-700x350-02.jpg" alt="website template image" class="img-fluid tm-img-2-col">
+          <table class="table table-striped">
+            <tbody>
+              <tr>
+                <td>Aliquam in nibh elementum</td>
+                <td>$100</td>
+              </tr>
+              <tr>
+                <td>Donec porta augue vitae egestas</td>
+                <td>$200</td>
+              </tr>
+              <tr>
+                <td>Nunc lacinia vehicula ipsum</td>
+                <td>$300</td>
+              </tr>
+              <tr>
+                <td>Porttitor rhoncus vel vitae libero</td>
+                <td>$400</td>
+              </tr>
+              <tr>
+                <td>Nulla neque ligula, bibendum vitae</td>
+                <td>$500</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="tm-section" id="tm-section-4">
+      <div class="col-md-4 margin-bottom-sm-3">
+        <div class="tm-3-col-box"><img src="../assets/img/tm-500x250-01.jpg" alt="website template image" class="img-fluid">
+          <div class="tm-description-box">
+            <p class="tm-description-text"> Proin auctor tortor quis ipsum dignissim, quis congue tortor scelerisque. Etiam malesuada efficitur elit, ut pellentesque mi pellentesque ut.</p>
+            <div class="tm-flex-center"><a href="https://samuelbetio.github.io/SamuelBetioNewsFeed/free-css-templates" class="btn btn-default tm-normal-btn tm-blue-btn">Read</a> <a href="https://samuelbetio.github.io/SamuelBetioNewsFeed/free-css-templates" class="btn btn-default tm-normal-btn tm-blue-bordered-btn">Read</a></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 margin-bottom-sm-3">
+        <div class="tm-3-col-box"><img src="../assets/img/tm-500x250-02.jpg" alt="website template image" class="img-fluid">
+          <div class="tm-description-box">
+            <p class="tm-description-text"> Proin auctor tortor quis ipsum dignissim, quis congue tortor scelerisque. Etiam malesuada efficitur elit, ut pellentesque mi pellentesque ut.</p>
+            <div class="tm-flex-center"><a href="https://samuelbetio.github.io/SamuelBetioNewsFeed/free-css-templates" class="btn btn-default tm-normal-btn tm-gray-btn">Read</a> <a href="https://samuelbetio.github.io/SamuelBetioNewsFeed/free-css-templates" class="btn btn-default tm-normal-btn tm-gray-bordered-btn">Read</a></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="tm-3-col-box"><img src="../assets/img/tm-500x250-03.jpg" alt="website template image" class="img-fluid">
+          <div class="tm-description-box">
+            <p class="tm-description-text"> Proin auctor tortor quis ipsum dignissim, quis congue tortor scelerisque. Etiam malesuada efficitur elit, ut pellentesque mi pellentesque ut.</p>
+            <div class="tm-flex-center"><a href="https://samuelbetio.github.io/SamuelBetioNewsFeed/free-css-templates" class="btn btn-default tm-normal-btn tm-green-btn">Read</a> <a href="https://samuelbetio.github.io/SamuelBetioNewsFeed/free-css-templates" class="btn btn-default tm-normal-btn tm-green-bordered-btn">Read</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="tm-section" id="tm-section-5">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <h3 class="blue-text">Full-width large table</h3>
+        <table class="table table-striped tm-full-width-large-table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Category</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Row 1</td>
+              <td>In consectetur et dolor eget mollis</td>
+              <td>First</td>
+              <td>$1,000</td>
+            </tr>
+            <tr>
+              <td>Row 2</td>
+              <td>Donec porta augue vitae egestas eleifend</td>
+              <td>First</td>
+              <td>$1,500</td>
+            </tr>
+            <tr>
+              <td>Row 3</td>
+              <td>Aliquam in nibh elementum</td>
+              <td>Second</td>
+              <td>$2,000</td>
+            </tr>
+            <tr>
+              <td>Row 4</td>
+              <td>Donec porta augue vitae egestas</td>
+              <td>Second</td>
+              <td>$2,500</td>
+            </tr>
+            <tr>
+              <td>Row 5</td>
+              <td>Nunc lacinia vehicula upsum</td>
+              <td>Second</td>
+              <td>$3,000</td>
+            </tr>
+            <tr>
+              <td>Row 6</td>
+              <td>Porttitor rhoncus vel vitae libero</td>
+              <td>Third</td>
+              <td>$3,500</td>
+            </tr>
+            <tr>
+              <td>Row 7</td>
+              <td>Nulla neque ligula, bibendum vitae</td>
+              <td>Third</td>
+              <td>$4,000</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+      <p class="text-xs-center tm-footer-text">Copyright &copy; 2016 Company Name</p>
+    </div>
+  </div>
+</div>
+<script src="../assets/js/jquery-1.11.3.min.js"></script>
+<script src="../assets/js/tether.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+<script src="../assets/js/jquery.singlePageNav.min.js"></script>
+<script src="../assets/js/custom-2.js"></script>
+<div id="freecssfooter">
+  <div id="fcssholder">
+    <div id="bsap_2365" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+    <div id="bsap_1635" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+    <div id="bsap_1279659" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+  </div>
+</div>
+<script type="text/javascript">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-72183449-1', 'auto');ga('send', 'pageview');</script>
+</body>
+</html>
